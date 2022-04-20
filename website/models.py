266f5bf2bbcefbb54298eaff0115aa86507468d3
Ritik Raj <ritikraj2699@gmail.com>
2022-04-21 00:06:07 +0530

    created database for movie list

--- website/models.py
+++ website/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class movieList(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=400)
+    releaseDate = models.DateField()
+    numberOfActors = models.IntegerField()
+
+    def __str__(self):
+        return self.title + ' ' + self.description
+
+
